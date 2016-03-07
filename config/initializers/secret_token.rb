0e919f8c6d053dbbb7b6575dee1b7c23728bcf74
kai <kai@KAI>
2016-03-07 14:53:28 +0900

    modify secret token file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,1 @@
-Rails.application.config.secret_key_base = secure_token
-
-def secure_token
-  "6eff5727818f9f605e8d84f72ae6bae74ddfeb65359f455865f33e85157cae263784c289e127d9338bd397979c06e533fba9704bc7cde458ce5c80fbad7956b4"
-end
+Fleamarket::Application.config.secret_key_base = "6eff5727818f9f605e8d84f72ae6bae74ddfeb65359f455865f33e85157cae263784c289e127d9338bd397979c06e533fba9704bc7cde458ce5c80fbad7956b4"
