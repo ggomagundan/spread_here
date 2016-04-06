92fabda471a48d79c386840b537f6106cebe20bd
ggomagundan <pbs52@hanmail.net>
2016-04-06 15:57:05 +0900

    modify login pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324083652) do
+ActiveRecord::Schema.define(version: 20160404083624) do
 
   create_table "flea_markets", force: true do |t|
     t.integer  "is_visible",              default: 0
@@ -47,15 +47,15 @@ ActiveRecord::Schema.define(version: 20160324083652) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username",               limit: 255
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
