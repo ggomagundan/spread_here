e46f2e19d6b7ed380947e268a88e7f42cc777feb
ggomagundan <pbs52@hanmail.net>
2016-05-09 16:30:22 +0900

    modify admin applujs file

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -15,7 +15,6 @@
 //= require jquery_nested_form
 //= require turbolinks
 //= require bootstrap
-// require summernote
 //= require moment.min
 //= require mdDateTimePicker.min
 //= require draggabilly.pkgd.min
