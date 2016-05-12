420a55a3fa08fd1756488be0d7e3407d0fdcb1e8
ggomagundan <pbs52@hanmail.net>
2016-05-12 20:48:25 +0900

    modify date search errors

--- app/controllers/flea_markets_controller.rb
+++ app/controllers/flea_markets_controller.rb
@@ -1,7 +1,7 @@
 class FleaMarketsController < ApplicationController
   def index
     @flea_markets = FleaMarket.searchable.page(params[:page]).per(6)
-    @flea_markets = @flea_markets.where("start_date <= ? and end_date >= ?",params[:date], params[:date]) if params[:date].present?
+    @flea_markets = @flea_markets.where("STR_TO_DATE(?, '%Y-%m-%d') between str_to_date(start_date, '%Y-%m-%d') and str_to_date(end_date, '%Y-%m-%d')", params[:date]) if params[:date].present?
   end
 
   def show
