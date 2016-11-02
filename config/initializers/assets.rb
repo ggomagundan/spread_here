ed5fe467dc7d4707a9a09533da278b80163d580f
ggomagundan <pbs52@hanmail.net>
2016-11-02 18:15:14 +0900

    modify admin_header and css file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( flea_markets.js )
 Rails.application.config.assets.precompile += %w( flea_markets.css )
+Rails.application.config.assets.precompile += %w( sessions.css )
 Rails.application.config.assets.precompile += %w(moment.js bootstrap-datetimepicker.js bootstrap-datetimepicker.css )
 
 Rails.application.config.assets.precompile += %w( admin/flea_markets.js admin/parsings.js admin/parse_configs.js)
